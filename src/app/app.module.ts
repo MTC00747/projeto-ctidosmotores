5569b00f37e3d9fc3d1f4906a8dd766dfa848fe8
Matheus Cavalcanti <Matheustcavalcanti@hotmail.com>
2023-06-30 17:59:03 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoAppComponent } from './logo-app/logo-app.component';
+import { HomeAppComponent } from './home-app/home-app.component';
+import { BannerComponent } from './banner/banner.component';
+import { InfoComponent } from './info/info.component';
+import { InformacaoComponent } from './informacao/informacao.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoAppComponent,
+    HomeAppComponent,
+    BannerComponent,
+    InfoComponent,
+    InformacaoComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
